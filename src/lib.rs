40477c42f972d742a45e07083d1d8d1ccae32c02
Jonathan Henderson <jkhenderson999@gmail.com>
2022-11-29 13:42:20 -0600

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,47 @@
+use std::{fs, io, path::Path};
+
+use toml::{macros::Deserialize, value::Table, Value};
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("std::io::error - {0}")]
+    Io(#[from] io::Error),
+
+    #[error("toml::de::Error - {0}")]
+    DeToml(#[from] toml::de::Error),
+
+    #[error("toml::ser::Error - {0}")]
+    SerToml(#[from] toml::ser::Error),
+}
+
+pub fn from_dir<'de, T, P>(path: P) -> Result<T, Error>
+where
+    T: Deserialize<'de>,
+    P: AsRef<Path>,
+{
+    let mut dir_table = Table::new();
+
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+        let path = entry.path();
+        let file_type = entry.file_type()?;
+        let file_name = entry.file_name().to_string_lossy().to_string();
+
+        let value = match file_type.is_dir() {
+            true => from_dir(&path)?,
+            false => from_file(&path)?,
+        };
+
+        dir_table.insert(file_name, value);
+    }
+
+    Ok(Value::Table(dir_table).try_into()?)
+}
+
+fn from_file<P>(path: P) -> Result<Value, Error>
+where
+    P: AsRef<Path>,
+{
+    let contents = fs::read_to_string(path)?;
+    Ok(toml::from_str(&contents)?)
+}
